15228d4bac3afb98eec94ad0e0b41a884543d93d
SezoR12 <134429202+SezoR12@users.noreply.github.com>
2026-06-27 08:28:50 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -121,6 +121,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Outlet />
+        <Toaster richColors position="top-center" />
       </AuthProvider>
     </QueryClientProvider>
   );
